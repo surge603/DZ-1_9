e7111181944f69afffb5f7a6573e1930e95507b9
Artem <40538552+surge603@users.noreply.github.com>
2022-03-02 21:29:36 +0300

    Задача 67

---
+++ tsk67/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 67: Показать натуральные числа от N до 1, N задано.
+// N = 5. -> "5, 4, 3, 2, 1"
+
+int n = 10;
+
+void ShowNatural(int i)
+{
+    if (i == 0) return;
+    Console.Write($"{i} ");
+    ShowNatural(i -1);
+}
+Console.Write($"N = {n}. -> ");
+ShowNatural(n);
+Console.WriteLine();
